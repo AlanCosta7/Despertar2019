c00e8de9d78d06bab626610228a0f86735aab23c
Alan Costa <alanpc7@gmail.com>
2018-08-18 19:14:55 -0300

    Update config.js

--- src/components/helpers/firebase/config.js
+++ src/components/helpers/firebase/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    apiKey: "AIzaSyBZnc-al51XODELJ5j32Mdqe8BpsSWmvWc",
-    authDomain: "despertar2019.firebaseapp.com",
-    databaseURL: "https://despertar2019.firebaseio.com",
-    projectId: "despertar2019",
-    storageBucket: "despertar2019.appspot.com",
-    messagingSenderId: "127706969910"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
     }
