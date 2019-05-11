a6deaeac3a4fc09686f9da2ee49738e6de9af149
Alan Pereira da Costa <alanpc7@gmail.com>
2019-05-10 21:08:25 -0300

    atualizar navegador

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -5,6 +5,8 @@
 
 import { register } from 'register-service-worker'
 
+window.__refreshing = false
+
 register(process.env.SERVICE_WORKER_FILE, {
   ready () {
     console.log('App is being served from cache by a service worker.')
@@ -15,8 +17,23 @@ register(process.env.SERVICE_WORKER_FILE, {
   cached (registration) { // registration -> a ServiceWorkerRegistration instance
     console.log('Content has been cached for offline use.')
   },
-  updated (registration) { // registration -> a ServiceWorkerRegistration instance
+  updated(registration) {
     console.log('New content is available; please refresh.')
+
+    // // registration -> a ServiceWorkerRegistration instance
+    // const installing = registration.installing
+    // const waiting = registration.waiting
+    // if (installing || waiting) {
+    //   console.info('waiting or installing...')
+    //   return
+    // }
+
+    if (window.confirm('Existe uma versão disponível. Deseja atualizar?')) {
+      // window.__refreshing = true
+      window.location.reload(true)
+    } else {
+      console.info('Nova versão ignorada pelo usuário.')
+    }
   },
   offline () {
     console.log('No internet connection found. App is running in offline mode.')
