74971454526620c2588c8ce91ae2562db7ec08e2
Alan Costa <alanpc7@gmail.com>
2019-05-31 16:41:30 -0300

    preletores

--- src/components/layout/base-social.vue
+++ src/components/layout/base-social.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="base-social flex flex-center">
-    <a class="q-mx-md" href="https://twitter.com/somosjbb" target="_blank"><img src="~assets/landpage/twitter.svg" alt="twitter" width="50"></a>
-    <a class="q-mx-md" href="https://www.instagram.com/somosjbb/" target="_blank"><img src="~assets/landpage/instagram.svg" alt="instagram" width="50"></a>
-    <a class="q-mx-md" href="https://www.facebook.com/somosjbb" target="_blank"><img src="~assets/landpage/facebook.svg" alt="facebook" width="50"></a>
-    <a class="q-mx-md" href="https://www.youtube.com/user/juventudebatista" target="_blank"><img src="~assets/landpage/youtube.svg" alt="youtube" width="50"></a>
+    <a class="q-mx-sm q-mb-lg" href="https://twitter.com/somosjbb" target="_blank"><img src="~assets/landpage/twitter.svg" alt="twitter" width="45"></a>
+    <a class="q-mx-sm q-mb-lg" href="https://www.instagram.com/somosjbb/" target="_blank"><img src="~assets/landpage/instagram.svg" alt="instagram" width="45"></a>
+    <a class="q-mx-sm q-mb-lg" href="https://www.facebook.com/somosjbb" target="_blank"><img src="~assets/landpage/facebook.svg" alt="facebook" width="45"></a>
+    <a class="q-mx-sm q-mb-lg" href="https://www.youtube.com/user/juventudebatista" target="_blank"><img src="~assets/landpage/youtube.svg" alt="youtube" width="45"></a>
   </div>
 </template>
 
@@ -12,3 +12,10 @@ export default {
   name: 'BaseSocial'
 }
 </script>
+
+<style>
+  .base-social {
+    padding: 0;
+    margin: 0
+  }
+</style>
